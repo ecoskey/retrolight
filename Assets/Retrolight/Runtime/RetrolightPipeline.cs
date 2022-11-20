c6262ae48fbf72884f192ade024ab977ddd54814
Emerson Coskey <56370779+emersoncoskey@users.noreply.github.com>
2022-11-19 18:01:07 -0800

    setup things + remove URP

---
+++ Assets/Retrolight/Runtime/RetrolightPipeline.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace Retrolight.Runtime {
+    public class RetrolightPipeline : RenderPipeline {
+        protected override void Render(ScriptableRenderContext context, Camera[] cameras) {
+            
+        }
+    }
+}
---
+++ Assets/Retrolight/Runtime/RetrolightPipelineAsset.cs
@@ -0,0 +1,6 @@
+using UnityEngine.Rendering;
+
+public class RetrolightPipelineAsset : RenderPipelineAsset
+{ 
+    protected override RenderPipeline CreatePipeline() { throw new System.NotImplementedException(); }
+}
