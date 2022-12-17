8554d663227541439c063f5d097b6a86c688a790
Emerson Coskey <56370779+emersoncoskey@users.noreply.github.com>
2022-12-17 11:38:28 -0800

    refactor and add texture utilities!

---
+++ Assets/Retrolight/Runtime/FrameData.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace Retrolight.Runtime {
+    public class FrameData {
+        public readonly Camera Camera;
+        public readonly CullingResults Cull;
+        public readonly RTHandleProperties RTHandleProperties;
+
+        public FrameData(Camera camera, CullingResults cull, RTHandleProperties rtHandleProperties) {
+            Camera = camera;
+            Cull = cull;
+            RTHandleProperties = rtHandleProperties;
+        }
+        
+    }
+}
