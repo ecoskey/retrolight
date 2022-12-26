3be377785c78a3eda442bb702538eb5a40e7d8d3
Emerson Coskey <56370779+emersoncoskey@users.noreply.github.com>
2022-12-25 17:55:42 -0800

    Dithering in progress!
    now, how to remove dither pattern on large, flat areas?

---
+++ Assets/Retrolight/ShaderLibrary/Filtering.hlsl
@@ -0,0 +1,32 @@
+#ifndef RETROLIGHT_DITHERING_INCLUDED
+#define RETROLIGHT_DITHERING_INCLUDED
+
+//bayer matrix values copied from Acerola on YT
+const int bayer2[2 * 2] = {
+    0, 2,
+    3, 1,
+};
+
+const int bayer4[4 * 4] = {
+    0,  8,  2,  10,
+    12, 4,  14, 6,
+    3,  11, 1,  9,
+    15, 7,  13, 5
+};
+
+const int bayer8[8 * 8] = {
+    0,  32, 8,  40, 2,  34, 10, 42,
+    48, 16, 56, 24, 50, 18, 58, 26,
+    12, 44, 4,  36, 14, 46, 6,  38,
+    60, 28, 52, 20, 62, 30, 54, 22,
+    3,  35, 11, 43, 1,  33, 9,  41,
+    51, 19, 59, 27, 49, 17, 57, 25,
+    15, 47, 7,  39, 13, 45, 5,  37,
+    63, 31, 55, 23, 61, 29, 53, 21
+};
+
+float Dither4(float value, float spread, uint2 pos) {
+    
+}
+
+#endif
